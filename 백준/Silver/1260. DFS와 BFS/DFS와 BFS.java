7b39742a173dfff696c3da8112fe9200f33bee19
DrivenByCode <jaykey1004@gmail.com>
2023-08-18 13:50:48 +0900

    [Silver II] Title: DFS와 BFS, Time: 272 ms, Memory: 27504 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -9,35 +9,37 @@ import java.util.StringTokenizer;
 public class Main {
     static int n, m, v;
     static boolean[] visited;
-    static int[][] map;
-    static String ans1 = "";
-    static String ans2 = "";
+    static int[][] adjMatrix;
 
-    static void DFS(int num) {
-        ans1 += num + " ";
+    static String DFS(int num) {
+        StringBuilder result = new StringBuilder();
+        result.append(num).append(" ");
         visited[num] = true;
         for (int next = 1; next <= n; next++) {
-            if (map[num][next] == 1 && !visited[next]) {
+            if (adjMatrix[num][next] == 1 && !visited[next]) {
                 visited[next] = true;
-                DFS(next);
+                result.append(DFS(next));
             }
         }
+        return result.toString();
     }
 
-    static void BFS(int num) {
+    static String BFS(int num) {
+        StringBuilder result = new StringBuilder();
         Queue<Integer> q = new LinkedList<>();
         q.add(num);
         while (!q.isEmpty()) {
             int now = q.poll();
             visited[now] = true;
-            ans2 += now + " ";
+            result.append(now).append(" ");
             for (int next = 1; next <= n; next++) {
-                if (map[now][next] == 1 && !visited[next]) {
+                if (adjMatrix[now][next] == 1 && !visited[next]) {
                     visited[next] = true;
                     q.add(next);
                 }
             }
         }
+        return result.toString();
     }
 
     public static void main(String[] args) throws IOException {
@@ -46,32 +48,22 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
-
         m = Integer.parseInt(st.nextToken());
-
         v = Integer.parseInt(st.nextToken());
 
         visited = new boolean[n + 1];
-
-        map = new int[n + 1][n + 1];
+        adjMatrix = new int[n + 1][n + 1];
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            map[a][b] = 1;
-            map[b][a] = 1;
+            adjMatrix[a][b] = 1;
+            adjMatrix[b][a] = 1;
         }
 
-        DFS(v);
-
-        System.out.println(ans1.trim());
-
+        System.out.println(DFS(v).trim());
         Arrays.fill(visited, false);
-
-        BFS(v);
-
-        System.out.println(ans2.trim());
-
+        System.out.println(BFS(v).trim());
     }
 }
