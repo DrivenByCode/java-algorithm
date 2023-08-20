c5daad1c50a90e9eaace084ce7833f4b0e1f304a
DrivenByCode <jaykey1004@gmail.com>
2023-08-20 12:22:22 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.99 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -2,15 +2,17 @@ import java.util.ArrayList;
 class Solution {
     public int solution(int n) {
         int idx = 1;
-        int[] noTree = new int[1000];
-        
-        for(int i = 1; i <= 1000; i++) {
+        int[] noTree = new int[10000];
+
+        // 최대 3x 부족의 수는 101이기 때문에 임의의 큰 수 10000으로 지정해놓고 101이 되면 종료
+        for(int i = 1; i <= 10000; i++) {
             String tmp = String.valueOf(i);
             if(i % 3 != 0) {
                 if(!tmp.contains("3")) {
                     noTree[idx++] = i;
                 }
             }
+            if(idx == 101) break;
         }
         return noTree[n];
     }
