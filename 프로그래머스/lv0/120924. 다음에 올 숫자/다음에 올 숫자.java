4e5eab70f05c88395e5554a5e84bc940921fe846
DrivenByCode <jaykey1004@gmail.com>
2023-09-04 19:14:08 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,20 +1,25 @@
 class Solution {
     static boolean getSeqType(int[] common) {
         int ratio = 0;
-        
+
+        // Runtime Error 방지: 0으로 나누는 예외 제외
         if(common[0] != 0) {
             ratio = common[1] / common[0];
         }
 
         if(common[1] * ratio == common[2]) {
             return true;
-        } 
+        }
         return false;
     }
     public int solution(int[] common) {
         int answer = 0;
-        if(common[0] != 0 && getSeqType(common)) {
-            int ratio = common[1] / common[0];
+        int ratio = 0;
+        // Runtime Error 방지: 0으로 나누는 예외 제외
+        if (getSeqType(common)) {
+            if (common[0] != 0) {
+                ratio = common[1] / common[0];
+            }
             answer = common[common.length - 1] * ratio;
         } else {
             int diff = common[1] - common[0];
