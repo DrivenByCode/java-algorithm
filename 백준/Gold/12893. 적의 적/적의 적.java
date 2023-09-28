00ab56a2a6c59790e7ba272da210b63588fdc75b
DrivenByCode <jaykey1004@gmail.com>
2023-09-28 16:54:25 +0900

    [Gold IV] Title: 적의 적, Time: 848 ms, Memory: 266156 KB -BaekjoonHub

--- 백준/Gold/12893. 적의 적/적의 적.java
+++ 백준/Gold/12893. 적의 적/적의 적.java
@@ -10,11 +10,15 @@ public class Main {
     private static ArrayList<Integer>[] adjList;
 
     private static boolean isBiparetteGraph(int currentVertex, int c) {
+        // 해당 학생을 1팀으로 하고
         teams[currentVertex] = c;
+        // 해당학생과 인접해있는 인접한 학생들을 모두 조사
         for (int nextVertex : adjList[currentVertex]) {
+            // 같은 현재 학생과 인접학생이 모두 같은 팀이면 이분 그래프가 아님
             if (teams[nextVertex] == c) {
                 return false;
             }
+            // 인접 학생이 팀이 없는데, 그 인접 학생도 인접학생의 인접학생과 팀이 같다면 이분 그래프가 아님
             if (teams[nextVertex] == 0 && !isBiparetteGraph(nextVertex, 3 - c)) {
                 return false;
             }
@@ -26,7 +30,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        // 정점(vertex)의 수
         int n = Integer.parseInt(st.nextToken());
+        // 간선(Edge)의 수
         int m = Integer.parseInt(st.nextToken());
 
         teams = new int[n + 1];
@@ -45,6 +51,7 @@ public class Main {
         }
 
         for (int k = 1; k <= n; k++) {
+            // k학생이 어느 팀인지 안 정해져 있다면
             if (teams[k] == 0) {
                 if (!isBiparetteGraph(k, 1)) {
                     System.out.println(0);
