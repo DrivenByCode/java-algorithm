2b6f8b2cb0e9f591780073d92f62e95dc148e308
DrivenByCode <jaykey1004@gmail.com>
2023-10-08 20:38:03 +0900

    [Gold V] Title: 트리, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -18,8 +18,7 @@ public class Main {
         }
     }
 
-    // dfs 원리를 이용하여 트리의 리프 노드 개수를 계산
-    // 삭제한 노드는 0으로 계산하여 더 이상 계산 안함
+    // dfs 원리를 이용하여 트리의 리프 노드(단말노드) 개수를 계산
     private static int countLeafNodes(int node) {
         if (deleted[node]) return 0;
 
@@ -30,7 +29,7 @@ public class Main {
             }
         }
 
-        // 자식 노드가 없거나 모든 자식 노드가 삭제된 경우 리프 노드로 카운트
+        // 자식 노드가 없거나 모든 자식 노드가 삭제된 경우 리프 노드로 카운트, 이게 리프노드(단말 노드)의 개념
         if (count == 0) return 1;
 
         return count;
@@ -60,7 +59,7 @@ public class Main {
         int deleteNodeNumber = Integer.parseInt(br.readLine());
         deleteNode(deleteNodeNumber);
 
-        // 루트 노드를 지웠으면 자식 노드 개수는 0
+        // 루트 노드를 지웠으면 개수는 0, 리프노드(단말노드)는 루트노드를 제외함.
         // 그렇지 않은 경우, 삭제 후 남은 리프 노드의 개수를 계산하여 출력
         if (deleteNodeNumber == root) {
             System.out.println(0);
