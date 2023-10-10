b07892cb543d5dd17997dab37a961b5a081e2d3e
DrivenByCode <jaykey1004@gmail.com>
2023-10-10 17:20:25 +0900

    [Silver I] Title: 접두사 찾기, Time: 452 ms, Memory: 51116 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.java
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.java
@@ -1,32 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    private static class TrieNode {
-        private HashMap<Character, TrieNode> children = new HashMap<>();
+    private static class TreeNode {
+        private TreeNode[] children = new TreeNode[26];
 
         private void insert(String str) {
-            TrieNode node = this;
+            // 루트 노드를 바라보게 함
+            TreeNode node = this;
             char[] chars = str.toCharArray();
             for (char c : chars) {
-                node.children.putIfAbsent(c, new TrieNode());
-                node = node.children.get(c);
+                if (node.children[c - 'a'] == null) {
+                    node.children[c - 'a'] = new TreeNode();
+                }
+                node = node.children[c - 'a'];
             }
         }
 
         private boolean contains(String str) {
-            TrieNode node = this;
+            // 루트 노드를 바라보게 함
+            TreeNode node = this;
             char[] chars = str.toCharArray();
             for (char c : chars) {
-                if (node.children.get(c) != null) {
-                    node = node.children.get(c);
-                } else {
+                // 만약 해당 자식노드가 비어있다면 접두사를 포함하고 있지 않은 것
+                // 즉시 false 리턴
+                if (node.children[c - 'a'] == null) {
                     return false;
                 }
+                node = node.children[c - 'a'];
             }
             return true;
         }
@@ -35,17 +38,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        TrieNode trie = new TrieNode();
+        TreeNode trie = new TreeNode();
 
         for (int i = 0; i < n; i++) {
-            String input = br.readLine();
-            trie.insert(input);
+            trie.insert(br.readLine());
         }
 
         int cnt = 0;
+
         for (int j = 0; j < m; j++) {
             if (trie.contains(br.readLine())) {
                 cnt++;
