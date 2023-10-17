60d064470a93edcf01411f3cd747031455eb63b6
DrivenByCode <jaykey1004@gmail.com>
2023-10-17 22:19:40 +0900

    [Gold V] Title: 토마토, Time: 692 ms, Memory: 119520 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -10,7 +10,7 @@ public class Main {
     private static final int[] dx = {0, 0, 0, 1, 0, -1}; // 아래 위 상 우 하 좌
     private static final int[] dy = {0, 0, 1, 0, -1, 0}; // 아래 위 상 우 하 좌
     private static final int[] dz = {-1, 1, 0, 0, 0, 0}; // 아래 위 상 우 하 좌
-    private static int n, m, h; // 세로, 가로, 높이
+    private static int n, m, h; // 세로(행), 가로(열), 높이
     private static Queue<Point> queue = new LinkedList<>();
 
     private static class Point {
@@ -42,7 +42,7 @@ public class Main {
                     int ny = py + dy[i];
                     int nz = pz + dz[i];
 
-                    if (1 <= nx && nx <= m && 1 <= ny && ny <= n && 1 <= nz && nz <= h) {
+                    if (0 <= nx && nx < n && 0 <= ny && ny < m && 0 <= nz && nz < h) {
                         if (map[nx][ny][nz] == 0) {
                             map[nx][ny][nz] = 1;
                             queue.add(new Point(nx, ny, nz));
@@ -56,9 +56,9 @@ public class Main {
 
         // 여기서는 모든 0이 1로 바뀌었는지 검사합니다.
         // 만약 아직도 0이 있다면 -1을 반환하며, 그렇지 않으면 days를 반환합니다.
-        for (int k = 1; k <= h; k++) {
-            for (int i = 1; i <= m; i++) {
-                for (int j = 1; j <= n; j++) {
+        for (int k = 0; k < h; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
                     if (map[i][j][k] == 0) {
                         return -1;
                     }
@@ -74,29 +74,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken()); // 세로
-        m = Integer.parseInt(st.nextToken()); // 가로
+        m = Integer.parseInt(st.nextToken()); // 가로(열)
+        n = Integer.parseInt(st.nextToken()); // 세로(행)
         h = Integer.parseInt(st.nextToken()); // 높이
 
-        map = new int[m + 1][n + 1][h + 1];
+        map = new int[n][m][h];
 
         // 층수
-        for (int k = 1; k <= h; k++) {
-            // 가로
-            for (int i = 1; i <= m; i++) {
+        for (int k = 0; k < h; k++) {
+            // 행(세로)
+            for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                // 세로
-                for (int j = 1; j <= n; j++) {
+                // 열(가로)
+                for (int j = 0; j < m; j++) {
                     map[i][j][k] = Integer.parseInt(st.nextToken());
                 }
             }
         }
 
-        for (int k = 1; k <= h; k++) {
+        // 층수
+        for (int k = 0; k < h; k++) {
             // 가로
-            for (int i = 1; i <= m; i++) {
+            for (int i = 0; i < n; i++) {
                 // 세로
-                for (int j = 1; j <= n; j++) {
+                for (int j = 0; j < m; j++) {
                     if (map[i][j][k] == 1) {
                         queue.add(new Point(i, j, k));
                     }
