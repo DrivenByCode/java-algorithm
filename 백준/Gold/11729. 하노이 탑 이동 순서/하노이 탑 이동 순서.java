74262b6f219b588efa67d5e8317a773bc456b96a
DrivenByCode <jaykey1004@gmail.com>
2023-10-19 11:49:32 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 424 ms, Memory: 35432 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -7,20 +7,23 @@ public class Main {
     private static int cnt;
     private static int n;
 
-    private static void hanoi(int level, int from, int temp, int to) {
-        if (level == n) return;
-        // 첫 번째 장대에서 두번쨰 장대로 옮김
-        hanoi(level + 1, from, to, temp);
+    private static void hanoi(int count, int from, int temp, int to) {
+        if (count == 0) return;
+        // 첫 번째 장대의 상위 count-1개의 원판을 두 번째 장대로 옮김
+        hanoi(count - 1, from, to, temp);
+
+        // 첫 번째 장대에 남아 있는 원판을 세 번째 장대로 옮김
         sb.append(from).append(" ").append(to).append("\n");
         cnt++;
-        // 두 번째 장대에서 세번쨰 장대로 옮김
-        hanoi(level + 1, temp, from, to);
+
+        // 두 번째 장대의 level-1개의 원판을 세 번째 장대로 옮김
+        hanoi(count - 1, temp, from, to);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine()); // 첫 번째 장대에 쌓인 원판의 수
-        hanoi(0, 1, 2, 3);
+        hanoi(n, 1, 2, 3);
         System.out.println(cnt);
         System.out.println(sb);
     }
