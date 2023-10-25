2ebe80dc76c885e755121327e94963d5cad8c154
DrivenByCode <jaykey1004@gmail.com>
2023-10-25 17:08:42 +0900

    [Silver IV] Title: 주몽, Time: 308 ms, Memory: 17252 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == m) {
+                    cnt++;
+                    // 오름 차순 정렬했으므로 m보다 크다면 break
+                } else if (arr[i] + arr[j] > m) {
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
