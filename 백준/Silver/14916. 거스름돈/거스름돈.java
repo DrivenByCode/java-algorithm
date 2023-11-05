c993aa3036ba455c5794956d91d02b51b97ea3de
DrivenByCode <jaykey1004@gmail.com>
2023-11-05 22:59:27 +0900

    [Silver V] Title: 거스름돈, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -2,43 +2,39 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static int getCoinsCount(int n) {
+    public static int getMinCount(int n) {
         int answer = 0;
 
-        while (n > 0) {
-            if (n >= 5) {
-                while (n % 5 == 0) {
-                    answer += n / 5;
-                    return answer;
+        if (n >= 5) {
+            if (n % 5 == 0) {
+                return n / 5;
+            }
+            int tmp = n;
+            int cnt = 0;
+            while (tmp >= 2) {
+                tmp -= 5;
+                cnt++;
+                // 11, 12, 13, 14, 16, 17,18, 19 등 여기 해당
+                if (0 < tmp && tmp < 10 && tmp % 2 == 0) {
+                    cnt += (tmp / 2);
+                    return cnt;
                 }
-                int tmp = n;
-                int cnt = 0;
-                while (tmp >= 2) {
-                    // 13, 17, 19 등
-                    tmp -= 5;
-                    cnt++;
-                    if (0 < tmp && tmp < 10 && tmp % 2 == 0) {
-                        cnt += (tmp / 2);
-                        return cnt;
-                    }
-                }
-                if (n % 2 == 0) {
-                    return n / 2;
-                }
-            } else if (n >= 2) {
-                n -= 2;
-                answer++;
-            } else {
-                return -1;
+            }
+            if (n % 2 == 0) {
+                return n / 2;
             }
         }
-        return answer;
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+        
+        return answer == 0 ? -1 : answer;
     }
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        System.out.println(getCoinsCount(n));
+        System.out.println(getMinCount(n));
     }
 }
