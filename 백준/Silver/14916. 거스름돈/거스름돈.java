3edb1d311aa507070c2ab7c6627f5fe6f265948d
DrivenByCode <jaykey1004@gmail.com>
2023-11-05 23:07:35 +0900

    [Silver V] Title: 거스름돈, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -1,10 +1,9 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
     public static int getMinCount(int n) {
-        int answer = 0;
-
         if (n >= 5) {
             if (n % 5 == 0) {
                 return n / 5;
@@ -14,26 +13,22 @@ public class Main {
             while (tmp >= 2) {
                 tmp -= 5;
                 cnt++;
-                // 11, 12, 13, 14, 16, 17,18, 19 등 여기 해당
                 if (0 < tmp && tmp < 10 && tmp % 2 == 0) {
                     cnt += (tmp / 2);
                     return cnt;
                 }
             }
-            if (n % 2 == 0) {
-                return n / 2;
-            }
         }
         if (n % 2 == 0) {
             return n / 2;
         }
-        
-        return answer == 0 ? -1 : answer;
+
+        return -1;
     }
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         System.out.println(getMinCount(n));
     }
