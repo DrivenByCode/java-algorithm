e976c6a7e797e91be64f015d595cd0068e1e0576
DrivenByCode <jaykey1004@gmail.com>
2023-11-16 16:24:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.29 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 56.2 MB, 시간: 5.73 ms
+메모리: 56.5 MB, 시간: 6.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 16:10:58
+2023년 11월 4일 16:24:13
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,51 @@
 import java.util.*;
+
 class Solution {
     private static boolean[][] visited;
     private static int row, col;
     private static final int[] dx = {0, 1, 0, -1};
     private static final int[] dy = {1, 0, -1, 0};
+
     private static class Point {
         private int x;
         private int y;
-        private int count;
-        private Point(int x, int y, int count) {
+        private Point(int x, int y) {
             this.x = x;
             this.y = y;
-            this.count = count;
         }
     }
+
     private static int bfs(int[][] maps) {
         Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(0, 0, 0));
+        queue.add(new Point(0, 0));
         visited[0][0] = true;
-        int min = Integer.MAX_VALUE;
+        int count = 1;
         while(!queue.isEmpty()) {
-            Point point = queue.poll();
-            int x = point.x;
-            int y = point.y;
+            int size = queue.size();
+            for (int s = 0; s < size; s++) {
+                Point point = queue.poll();
+                int x = point.x;
+                int y = point.y;
 
-            if(x == row-1 && y == col-1) {
-                min = Math.min(min, point.count + 1);
-            }
-            for(int i = 0; i < 4; i++) {
-
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(0 <= nx && nx < row && 0 <= ny && ny < col) {
-                    if(!visited[nx][ny] && maps[nx][ny] == 1) {
-                        visited[nx][ny] = true;
-                        queue.offer(new Point(nx,ny, point.count + 1));
+                if(x == row-1 && y == col-1) {
+                    return count;
+                }
+                for(int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if(0 <= nx && nx < row && 0 <= ny && ny < col) {
+                        if(!visited[nx][ny] && maps[nx][ny] == 1) {
+                            visited[nx][ny] = true;
+                            queue.offer(new Point(nx, ny));
+                        }
                     }
                 }
             }
+            count++;
         }
-        return min == Integer.MAX_VALUE? -1: min;
+        return -1;
     }
+
     public int solution(int[][] maps) {
         row = maps.length;
         col = maps[0].length;
