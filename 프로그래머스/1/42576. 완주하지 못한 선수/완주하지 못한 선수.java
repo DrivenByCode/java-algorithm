5fad6eae0d4e0da7b2298a1df42fc8e729652298
DrivenByCode <jaykey1004@gmail.com>
2023-11-16 16:53:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.83 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,20 +1,19 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> participantCount = new HashMap<>();
         for(String p : participant) {
             participantCount.put(p, participantCount.getOrDefault(p, 0) + 1);
         }
-        
+
         for(String c : completion) {
             participantCount.put(c, participantCount.get(c) - 1);
-        }
-        
-        for(Map.Entry<String, Integer> entry : participantCount.entrySet()) {
-            if(entry.getValue() > 0) {
-                return entry.getKey();
+            if (participantCount.get(c) == 0) {
+                participantCount.remove(c);
             }
         }
-        return "";
+
+        return participantCount.keySet().iterator().next();
     }
 }
