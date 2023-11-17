da6b3fd94cbe2480058290007af181ba6933b374
DrivenByCode <jaykey1004@gmail.com>
2023-11-17 15:05:19 +0900

    [Bronze V] Title: N 찍기, Time: 228 ms, Memory: 18732 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -7,8 +7,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i <= n; i++) {
-            System.out.println(i);
+            sb.append(i).append("\n");
         }
+
+        System.out.println(sb.toString().trim());
     }
 }
