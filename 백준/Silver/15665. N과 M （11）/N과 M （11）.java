c128bbb859a009db01cf09658b3813a8a100c124
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2023-11-25 14:20:10 +0900

    [Silver II] Title: N과 M (11), Time: 1216 ms, Memory: 185828 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int[] arr;
+    private static int[] selectedNumbers;
+    private static final StringBuilder sb = new StringBuilder();
+    private static final Set<String> generatedSequences = new HashSet<>();
+    private static int n, m;
+
+    private static void dfs(int level) {
+        if (level == m) {
+            String sequence = Arrays.toString(selectedNumbers);
+            if (!generatedSequences.contains(sequence)) {
+                generatedSequences.add(sequence);
+                for (int num : selectedNumbers) {
+                    sb.append(num).append(" ");
+                }
+                sb.append("\n");
+            }
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            selectedNumbers[level] = arr[i];
+            dfs(level + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+
+        selectedNumbers = new int[m];
+
+        dfs(0);
+
+        System.out.println(sb.toString().trim());
+    }
+}
