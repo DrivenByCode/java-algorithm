156f0333688badec1d35f6991337f30b78ba70af
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2023-12-07 16:40:45 +0900

    [Silver II] Title: 부분수열의 합, Time: 824 ms, Memory: 301364 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int answer = 0;
+        for (int i = 0; i < (1 << n); i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                // 5개의 원소가 있다고 하면
+                // [0, 0, 0, 0, 0]
+                // 여기서 0은 해당 숫자를 선택하지 않았음을, 1은 선택했음을 의미.
+                // 따라서 (1 << j)와 (1 << j) 미만 숫자가 i와 비트 AND 연산시 겹치는 게 있다면(0이 아니라면)
+                // 하나 이상을 선택했다는 의미
+                // 이 방식으로 부분집합의 총 갯수를 구할 수 있다.
+                if ((i & (1 << j)) != 0) {
+                    list.add(arr[j]);
+                }
+            }
+            if (list.stream().mapToInt(Integer::intValue).sum() == s) {
+                if (!list.isEmpty()) {
+                    answer++;
+                    list.clear();
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
