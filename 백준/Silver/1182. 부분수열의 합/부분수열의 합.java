e57798c872cb2723edffa181ed8ea1d8f7faecc1
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2023-12-07 16:58:02 +0900

    [Silver II] Title: 부분수열의 합, Time: 136 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -5,34 +5,37 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static int[] arr;
+    private static int count;
+    private static int n, s;
+
+    private static void calculateSubsequenceSum(int index, int sum) {
+        if (index == n) {
+            if (sum == s) {
+                count++;
+            }
+            return;
+        }
+        calculateSubsequenceSum(index + 1, sum + arr[index]);
+        calculateSubsequenceSum(index + 1, sum);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
 
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        int answer = 0;
-        for (int i = 0; i < (1 << n); i++) {
-            int sum = 0;
-            for (int j = 0; j < n; j++) {
-                // 5개의 원소가 있다고 하면
-                // [0, 0, 0, 0, 0]
-                // 여기서 0은 해당 숫자를 선택하지 않았음을, 1은 선택했음을 의미.
-                // 따라서 (1 << j)와 (1 << j) 미만 숫자가 i와 비트 AND 연산시 겹치는 게 있다면(0이 아니라면)
-                // 하나 이상을 선택했다는 의미
-                // 이 방식으로 부분집합의 총 갯수를 구할 수 있다.
-                if ((i & (1 << j)) != 0) {
-                    sum += arr[j];
-                }
-            }
-            if (sum == s) {
-                answer++;
-            }
+        calculateSubsequenceSum(0, 0);
+
+        // 공집합의 합일 경우 count 하나 제거
+        if (s == 0) {
+            count--;
         }
 
-        System.out.println(s == 0 ? answer - 1 : answer);
+        System.out.println(count);
     }
 }
