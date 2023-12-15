5326eb81fd4fe99aeef887e4a4ff25ed185fdfe9
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2023-12-15 12:43:46 +0900

    [Gold V] Title: 동전 2, Time: 140 ms, Memory: 14636 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[100001];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        for (int i = 0; i < n; i++) {
+            dp[coins[i]] = 1;
+            for (int j = coins[i]; j <= k; j++) {
+                if (dp[j - coins[i]] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[k] == Integer.MAX_VALUE ? -1 : dp[k]);
+    }
+}
