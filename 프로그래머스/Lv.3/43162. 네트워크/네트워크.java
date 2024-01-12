fe08636dfef5a92b64cb4e455792c28f395431cc
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2024-01-12 21:18:36 +0900

    [level Lv.3] Title: 네트워크, Time: 0.22 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/43162. 네트워크/네트워크.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    private static boolean[] visited;
+
+    private static int bfs(int[][] computers, int startNode) {
+        int n = computers.length;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(startNode);
+        visited[startNode] = true;
+
+        while (!queue.isEmpty()) {
+            int currentNode = queue.poll();
+            
+            // for(int nextNode : computers[currentNode]) 이 형태는 연결 여부만 나타내는 것(연결됨:1, 연결안됨:0) 따라서 사용X
+            for (int nextNode = 0; nextNode < n; nextNode++) {
+                if (computers[currentNode][nextNode] == 1 && !visited[nextNode]) {
+                    visited[nextNode] = true;
+                    queue.offer(nextNode);
+                }
+            }
+        }
+
+        return 1;
+    }
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer += bfs(computers, i);
+            }
+        }
+
+        return answer;
+    }
+}
