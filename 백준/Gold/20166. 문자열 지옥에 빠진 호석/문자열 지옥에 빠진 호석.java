49359700da7765f1da22fb56cabdc2326b037871
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2024-03-15 15:22:51 +0900

    [Gold IV] Title: 문자열 지옥에 빠진 호석, Time: 536 ms, Memory: 72344 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.java
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.java
@@ -1,38 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     private static char[][] map;
-    private static HashMap<Integer, Integer> hasingTargetWords = new HashMap<>();
+    private static final HashMap<Integer, Integer> hasingTargetWords = new HashMap<>();
     private static final int[] dRow = {0, -1, 0, 1, -1, 1, -1, 1};
     private static final int[] dCol = {-1, 0, 1, 0, -1, 1, 1, -1};
+    private static int[] powers = new int[6];
+
     private static int n, m;
 
-    private static void dfs(int x, int y, String word, int depth, HashMap<Integer, Integer> foundWords) {
-        if (depth > 5) return; // 문자열 최대 길이 조절
+    private static class Node {
+        int row, col, hash, depth;
 
-        word += map[x][y];
-        int hash = hash(word);
-
-        if (hasingTargetWords.containsKey(hash)) {
-            foundWords.put(hash, foundWords.getOrDefault(hash, 0) + 1);
+        public Node(int row, int col, int depth, int hash) {
+            this.row = row;
+            this.col = col;
+            this.depth = depth;
+            this.hash = hash;
         }
+    }
 
-        for (int i = 0; i < 8; i++) {
-            int nx = (x + dRow[i] + n) % n;
-            int ny = (y + dCol[i] + m) % m;
-            dfs(nx, ny, word, depth + 1, foundWords);
+    private static void bfs(int row, int col) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(row, col, 1, map[row][col] - 'a' + 1)); // 초기 위치와 해시값
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            int hash = current.hash;
+
+            // 현재 해시값으로 찾은 단어의 개수를 1 증가시킴
+            if (hasingTargetWords.containsKey((hash))) {
+                hasingTargetWords.put(hash, hasingTargetWords.get(hash) + 1);
+            }
+
+            int nextDepth = current.depth + 1;
+            if (current.depth < 5) { // 깊이 제한 (문제에 따라 조정)
+                for (int i = 0; i < 8; i++) {
+                    int nextRow = (current.row + dRow[i] + n) % n;
+                    int nextCol = (current.col + dCol[i] + m) % m;
+                    int nextHash = hash + ((map[nextRow][nextCol] - 'a' + 1) * powers[nextDepth]);
+                    queue.offer(new Node(nextRow, nextCol, nextDepth, nextHash));
+                }
+            }
         }
     }
 
     private static int hash(String s) {
-        int hash = 0, power = 1;
-        for (char c : s.toCharArray()) {
-            hash += (c - 'a' + 1) * power;
-            power <<= 5;
+        int hash = 0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            hash += (chars[i] - 'a' + 1) * powers[i + 1];
         }
         return hash;
     }
@@ -45,6 +65,12 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
+        int powerInit = 1;
+        for (int i = 1; i <= 5; i++) {
+            powers[i] = powerInit;
+            powerInit <<= 5;
+        }
+
         map = new char[n][m];
 
         for (int i = 0; i < n; i++) {
@@ -52,21 +78,22 @@ public class Main {
             map[i] = input.toCharArray();
         }
 
+        List<Integer> targetWords = new ArrayList<>();
         for (int i = 0; i < k; i++) {
             String input = br.readLine();
             int hash = hash(input);
-            hasingTargetWords.put(hash, hasingTargetWords.getOrDefault(hash, 0) + 1);
+            hasingTargetWords.put(hash, 0);
+            targetWords.add(hash);
         }
 
-        HashMap<Integer, Integer> foundWords = new HashMap<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                dfs(i, j, "", 0, foundWords);
+                bfs(i, j);
             }
         }
 
-        for (int hash : hasingTargetWords.keySet()) {
-            System.out.println(foundWords.getOrDefault(hash, 0));
+        for (int hash : targetWords) {
+            System.out.println(hasingTargetWords.getOrDefault(hash, 0));
         }
     }
 }
