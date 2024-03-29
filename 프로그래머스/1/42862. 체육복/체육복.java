5d1e93d916596be31a75c53a86efeba606777b79
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2024-03-29 16:47:39 +0900

    [level 1] Title: 체육복, Time: 0.48 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        // 여벌 체육복을 가져왔지만 뺏긴 경우
+        for(int i = 0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                if(lost[i] == reserve[j]) {
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                }
+            }
+        }
+        
+        // 도난 당했지만 체육복을 빌릴 수 있는 경우
+        for(int i = 0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                if(lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
