414cd708b589dee19e9ba30f75599a3fed911773
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2024-07-01 17:46:00 +0900

    [Bronze II] Title: 5의 수난, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/23037. 5의 수난/5의 수난.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int answer = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            answer += (int) Math.pow(input.charAt(i) - '0', 5);
+        }
+
+        System.out.println(answer);
+    }
+}
