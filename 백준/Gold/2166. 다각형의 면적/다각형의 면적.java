410c683e2eaa17d18c64261bb8bc2a48c9e29d47
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2024-08-14 16:56:12 +0900

    [Gold V] Title: 다각형의 면적, Time: 220 ms, Memory: 25808 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int[][] points;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        points = new int[n][2];
+
+        StringTokenizer st;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            points[i][0] = x;
+            points[i][1] = y;
+        }
+
+        // Shoelace 공식에 따라 넓이 계산
+        BigInteger area = BigInteger.ZERO;
+
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            int x2 = points[(i + 1) % n][0];
+            int y2 = points[(i + 1) % n][1];
+
+            BigInteger term1 = BigInteger.valueOf(x1).multiply(BigInteger.valueOf(y2));
+            BigInteger term2 = BigInteger.valueOf(y1).multiply(BigInteger.valueOf(x2));
+
+            area = area.add(term1).subtract(term2);
+        }
+
+        // 절대값을 취하고 2로 나누기
+        area = area.abs();
+
+        // BigDecimal로 변환하여 소수점 계산
+        BigDecimal finalArea = new BigDecimal(area).divide(BigDecimal.valueOf(2), 10, RoundingMode.HALF_UP);
+
+        // 소수점 첫째 자리까지 반올림하여 출력
+        System.out.printf("%.1f\n", finalArea.setScale(1, RoundingMode.HALF_UP));
+    }
+}
