a0347d8b8c9e2a03ac7d26faa9ebc7d842ccc19b
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2024-12-18 23:36:58 +0900

    [Gold II] Title: 중앙값 구하기, Time: 180 ms, Memory: 16296 KB -BaekjoonHub

---
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] inputs = new int[n];
+            int idx = 0;
+
+            while (idx < n) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                while (st.hasMoreTokens() && idx < n) {
+                    inputs[idx++] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+            List<Integer> result = new ArrayList<>();
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < n; j++) {
+                if (maxHeap.isEmpty() || inputs[j] <= maxHeap.peek()) {
+                    maxHeap.offer(inputs[j]);
+                } else {
+                    minHeap.offer(inputs[j]);
+                }
+
+                // 힙 밸런싱 : 항상 최대 힙의 크기가 최소힙과 같거나 1더 큼
+                if (maxHeap.size() > minHeap.size() + 1) {
+                    minHeap.offer(maxHeap.poll());
+                } else if (minHeap.size() > maxHeap.size()) {
+                    maxHeap.offer(minHeap.poll());
+                }
+
+                // 홀수 번째일 때 중앙값 추가 (j는 0부터 시작)
+                if (j % 2 == 0) {
+                    result.add(maxHeap.peek());
+                }
+            }
+
+            sb.append(result.size()).append("\n");
+            for (int k = 0; k < result.size(); k++) {
+                sb.append(result.get(k)).append(" ");
+                // 10번 째 숫자일 때마다 개행
+                if ((k + 1) % 10 == 0) sb.append("\n");
+            }
+
+            System.out.println(sb.toString().trim());
+        }
+    }
+}
