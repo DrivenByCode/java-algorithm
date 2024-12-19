eb1f942faac380d67fd2f691bd84bfee526fb4da
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2024-12-19 11:10:08 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 600 ms, Memory: 37208 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -3,6 +3,25 @@ import java.util.*;
 
 public class Main {
     private static final Map<String, Integer> wordCount = new HashMap<>();
+    private static class WordInfo implements Comparable<WordInfo> {
+        private final String word;
+        private final int wordCount;
+        public WordInfo(String word, int wordCount) {
+            this.word = word;
+            this.wordCount = wordCount;
+        }
+
+        @Override
+        public int compareTo(WordInfo other) {
+            if(this.wordCount == other.wordCount) {
+                if(this.word.length() == other.word.length()) {
+                    return this.word.compareTo(other.word);
+                }
+                return other.word.length() - this.word.length();
+            }
+            return other.wordCount - this.wordCount;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,31 +30,25 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        Set<String> w = new HashSet<>();
-
-        TreeSet<String> words = new TreeSet<>((o1, o2) -> {
-            if(wordCount.get(o1) == wordCount.get(o2)) {
-                if(o1.length() == o2.length()) {
-                    return o1.compareTo(o2);
-                }
-                return o2.length() - o1.length();
-            }
-            return wordCount.get(o2) - wordCount.get(o1);
-        });
+        PriorityQueue<WordInfo> words = new PriorityQueue<>();
 
         for(int i  = 0; i < n; i++) {
             String word = br.readLine();
             if(word.length() < m) continue;
             wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
-            w.add(word);
         }
 
-        words.addAll(w);
+        for(Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            String word = entry.getKey();
+            int wordCount = entry.getValue();
+
+            words.offer(new WordInfo(word, wordCount));
+        }
 
         StringBuilder sb = new StringBuilder();
 
         while(!words.isEmpty()) {
-            sb.append(words.pollFirst()).append("\n");
+            sb.append(words.poll().word).append("\n");
         }
 
         bw.write(sb.toString());
