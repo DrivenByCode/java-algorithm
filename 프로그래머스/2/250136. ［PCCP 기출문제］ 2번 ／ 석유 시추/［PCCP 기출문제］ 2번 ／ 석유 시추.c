3bbb8ad3d1684393a91ea7203afe42047f63d24e
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2025-01-26 17:37:23 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 59.97 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// land_rows는 2차원 배열 land의 행 길이, land_cols는 2차원 배열 land의 열 길이입니다.
+int solution(int** land, size_t land_rows, size_t land_cols) {
+    int answer = 0;
+    return answer;
+}
