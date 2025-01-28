974e877996643adaeebc36572cb59345698748bc
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2025-01-28 14:36:42 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.06 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,32 +1,27 @@
 class Solution {
     public int solution(int[] wallet, int[] bill) {
-        int answer = 0;
-        
-        int maxBill = Math.max(bill[0], bill[1]);
-        int minBill = Math.min(bill[0], bill[1]);
-
-        bill[0] = Math.max(maxBill, minBill);
-        bill[1] = Math.min(maxBill, minBill);
-        
         int maxWallet = Math.max(wallet[0], wallet[1]);
         int minWallet = Math.min(wallet[0], wallet[1]);
         
-        while(true) {           
-            if(bill[0] <= maxWallet && bill[1] <= minWallet) {
+        int currentMaxBill = Math.max(bill[0], bill[1]);
+        int currentMinBill = Math.min(bill[0], bill[1]);
+        
+        int answer = 0;
+        
+        while (true) {
+            if (currentMaxBill <= maxWallet && currentMinBill <= minWallet) {
                 break;
             }
             
-            maxBill = Math.max(bill[0], bill[1]) / 2;
-            minBill = Math.min(bill[0], bill[1]);
-
-            bill[0] = Math.max(maxBill, minBill);
-            bill[1] = Math.min(maxBill, minBill);
-
-            maxWallet = Math.max(wallet[0], wallet[1]);
-            minWallet = Math.min(wallet[0], wallet[1]);
+            int splitPart = currentMaxBill / 2;
+            int otherPart = currentMinBill;
+            
+            currentMaxBill = Math.max(splitPart, otherPart);
+            currentMinBill = Math.min(splitPart, otherPart);
             
             answer++;
         }
+        
         return answer;
     }
 }
