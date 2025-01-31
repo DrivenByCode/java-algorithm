4eff3b20244d40e7a013cf6c2079e77b1a0132f3
DrivenByCode <148737693+DrivenByCode@users.noreply.github.com>
2025-01-31 20:17:56 +0900

    [Gold IV] Title: 여행 가자, Time: 164 ms, Memory: 19028 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -12,56 +12,6 @@ public class Main {
     private static int[] rank;
     private static int[] parent;
 
-    private static void initUnionFind() {
-        rank = new int[n + 1];
-        parent = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            rank[i] = 1;
-            parent[i] = i;
-        }
-    }
-
-    private static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    private static void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX == rootY) return;
-
-        if (parent[rootX] >= parent[rootY]) {
-            parent[rootY] = rootX;
-            if (parent[rootX] == parent[rootY]) {
-                rank[rootX]++;
-            }
-        } else {
-            parent[rootX] = rootY;
-        }
-    }
-
-    private static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        visited[start] = true;
-
-        while (!q.isEmpty()) {
-            int currentVertex = q.poll();
-            for (int nextVertex = 1; nextVertex <= n; nextVertex++) {
-                if (map[currentVertex][nextVertex] == 1 && !visited[nextVertex]) {
-                    q.add(nextVertex);
-                    visited[nextVertex] = true;
-                    // 인접해 있는 여행지 경로끼리는 같은 집합에 넣어야 함
-                    if (find(currentVertex) != find(nextVertex)) {
-                        union(currentVertex, nextVertex);
-                    }
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
@@ -102,4 +52,55 @@ public class Main {
         }
         System.out.println("YES");
     }
+    
+     private static void initUnionFind() {
+        rank = new int[n + 1];
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            rank[i] = 1;
+            parent[i] = i;
+        }
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) return;
+
+        if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else if(rank[rootX] <= rank[rootY]) {
+            parent[rootX] = rootY;
+            if(rank[rootX] == rank[rootY]) {
+                rank[rootX]++;
+            }
+        } 
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int currentVertex = q.poll();
+            for (int nextVertex = 1; nextVertex <= n; nextVertex++) {
+                if (map[currentVertex][nextVertex] == 1 && !visited[nextVertex]) {
+                    q.add(nextVertex);
+                    visited[nextVertex] = true;
+                    // 인접해 있는 여행지 경로끼리는 같은 집합에 넣어야 함
+                    if (find(currentVertex) != find(nextVertex)) {
+                        union(currentVertex, nextVertex);
+                    }
+                }
+            }
+        }
+    }
+
 }
